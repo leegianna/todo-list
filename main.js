14b19b93b49da84543198dcbafbb0646b2f9a47e
Gianna Lee <giannaleedev@gmail.com>
2025-04-30 16:13:34 +0900

    feat: implement edit, delete, and check toggle features

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
+const editBtn = document.querySelector('.item__edit');
+const deleteBtn = document.querySelector('.item__delete');
 
 function onAdd() {
   const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
+  }
   const itemRow = createItem(text);
   items.appendChild(itemRow);
   input.value = '';
@@ -31,6 +37,43 @@ function createItem(text) {
   deleteBtn.setAttribute('class', 'item__delete');
   deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
 
+  checkBox.addEventListener('change', () => {
+    if (checkBox.checked) {
+      name.classList.add('checked');
+    } else {
+      name.classList.remove('checked');
+    }
+  });
+
+  editBtn.addEventListener('click', () => {
+    const name = itemRow.querySelector('.item__name');
+    const edit = document.createElement('input');
+    edit.setAttribute('type', 'text');
+    edit.setAttribute('value', name.innerText);
+    edit.setAttribute('class', 'edit__input');
+
+    itemRow.querySelector('.item').replaceChild(edit, name);
+
+    edit.focus();
+    edit.setSelectionRange(edit.value.length, edit.value.length);
+
+    edit.addEventListener('keypress', (event) => {
+      if (text === '') {
+        input.focus();
+        return;
+      }
+      if (event.key === 'Enter') {
+        name.innerText = edit.value;
+        itemRow.querySelector('.item').replaceChild(name, edit);
+        edit.remove();
+      }
+    });
+  });
+
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+
   const item = document.createElement('div');
   item.setAttribute('class', 'item');
 
@@ -46,4 +89,10 @@ function createItem(text) {
 
 addBtn.addEventListener('click', () => {
   onAdd();
+});
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
 });
